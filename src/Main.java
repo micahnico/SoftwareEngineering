8d130732df45820310a05e0a190d09e8e928e0fe
homan-jonathan <homanjp20@gcc.edu>
2023-02-01 14:36:26 -0500

    updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World, from Jonathan's branch");
+    print();
+  }
+  public static void print(){
+    System.out.println("Hello World");
   }
 }
